ef0adde83b5b5e2735809b67480e906fab11ef40
rabelmervin <rabelmervin@gmail.com>
2025-07-12 23:21:58 +0530

    implementation of jwt token logic

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,12 +4,17 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["id", "email", "username", "password"]
         extra_kwargs = {
             "password": {"write_only": True}
         }
 
-def create(self, validated_data):
-    user = User.objects.create_user(**validated_data)
-    user.save()
-    return user
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"]
+        )
+        user.is_active = True
+        user.save()
+        return user
